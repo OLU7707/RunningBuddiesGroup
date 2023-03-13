97505a15ea52a2fd358d53f7011271756720fc93
Natheem Mohamed <natheem_mohamed98@hotmail.com>
2023-03-13 13:37:12 +0000

    test commit

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ const db = require('./services/db');
 
 // Create a route for root - /
 app.get("/", function(req, res) {
-    res.send("Hello world!");
+    res.send("Hello world! Natheem");
 });
 
 // Create a route for testing the db
