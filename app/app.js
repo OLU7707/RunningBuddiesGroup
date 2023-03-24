6560f78b99dd2ebb3ae1c280128a0a82140af209
Natheem1 <124581559+Natheem1@users.noreply.github.com>
2023-03-24 14:43:55 +0000

    making changes

--- app/app.js
+++ app/app.js
@@ -12,7 +12,7 @@ const db = require('./services/db');
 
 // Create a route for root - / TEST
 app.get("/", function(req, res) {
-    res.send("Hello world! Natheem is Married");
+    res.send("Hello world! Natheem ");
 });
 
 //1. Displaying All Runners 
